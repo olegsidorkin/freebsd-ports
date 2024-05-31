f48f6ff6e45b0175d4c303641589ddf95269e796
Oleg Sidorkin <osidorkin@gmail.com>
2024-05-31 23:12:12 +0300

    www/qt5-webengine: Fix build with python3.12

---
+++ www/qt5-webengine/files/patch-src_3rdparty_chromium_mojo_public_tools_mojom_mojom_parse_lexer.py
@@ -0,0 +1,10 @@
+--- src/3rdparty/chromium/mojo/public/tools/mojom/mojom/parse/lexer.py.orig	2024-05-31 17:04:03 UTC
++++ src/3rdparty/chromium/mojo/public/tools/mojom/mojom/parse/lexer.py
+@@ -2,7 +2,6 @@
+ # Use of this source code is governed by a BSD-style license that can be
+ # found in the LICENSE file.
+ 
+-import imp
+ import os.path
+ import sys
+ 
