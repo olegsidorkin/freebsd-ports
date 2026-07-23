3230139bc3f4dae6769d615067c693d65cb7c977
Oleg Sidorkin <osidorkin@gmail.com>
2026-07-23 19:41:18 +0300

    textproc/redland: Use newer bdb

--- textproc/redland/files/patch-src_rdf__storage__mysql.c
+++ textproc/redland/files/patch-src_rdf__storage__mysql.c
@@ -1,6 +1,14 @@
---- src/rdf_storage_mysql.c.orig	2023-11-25 15:00:04 UTC
+--- src/rdf_storage_mysql.c.orig	2012-12-29 23:01:17 UTC
 +++ src/rdf_storage_mysql.c
-@@ -447,7 +447,7 @@ librdf_storage_mysql_get_handle(librdf_storage* storag
+@@ -38,6 +38,7 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
++#include <stdbool.h>
+ #ifdef HAVE_STDLIB_H
+ #include <stdlib.h>
+ #endif
+@@ -447,7 +448,7 @@ librdf_storage_mysql_get_handle(librdf_storage* storag
  
  #ifdef HAVE_MYSQL_OPT_RECONNECT
    if(1) {
